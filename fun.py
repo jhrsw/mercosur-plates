b40c3ee32e9ced27051a77ae7a3e653dd9658fb0
João <joaohirasawaa@gmail.com>
2018-12-17 14:22:29 -0200

    Finalizado

---
+++ fun.py
@@ -0,0 +1,59 @@
+from unicodedata import normalize
+
+def chopchopextra(string):
+    if "/" in string:
+        resto = string.split('/', 1)[0]
+    else:
+        resto = string.rstrip("\n")
+
+    return resto.upper()
+
+
+def chopchopsete(dic):
+    return [word for word in dic if (len(word) == 7 and '-' not in word and '\'' not in word)]
+
+
+def chopchopacentos(string):
+    return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
+
+
+# Formato das palavras: AAA#A##
+# 1 - (I), L
+# 2 - Z
+# 3 - E
+# 4 - (A), H
+# 5 - S
+# 6 - B, (G)
+# 7 - (T), J
+# 8 - B
+# 9 - (Q), G
+# 0 - O
+
+letras = set('IZEASBTBQO')
+
+def chopchopleet(dic):
+    return [word for word in dic if any(i in word[3] for i in letras) and any(i in word[5] for i in letras) and any(i in word[6] for i in letras)]
+
+
+correspondentes = {
+    'I':'1',
+    'Z':'2',
+    'E':'3',
+    'A':'4',
+    'S':'5',
+    'G':'6',
+    'T':'7',
+    'B':'8',
+    'Q':'9',
+    'O':'0'
+}
+
+
+def final(string):
+    lista = list(string)
+    lista[3] = correspondentes.get(string[3])
+    lista[5] = correspondentes.get(string[5])
+    lista[6] = correspondentes.get(string[6])
+
+    return ''.join(lista)
+
--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-def chopchopextra(string):
-    if "/" in string:
-        resto = string.split('/', 1)[0]
-    else:
-        resto = string.rstrip("\n")
-
-    return resto.upper()
-
-
-def chopchopsete(dic):
-    return [word for word in dic if (len(word) == 7 and '-' not in word)]
-
-
-# AAA#A##
-# 1 - (I), L
-# 2 - Z
-# 3 - E
-# 4 - (A), H
-# 5 - S
-# 6 - B, (G)
-# 7 - (T), J
-# 8 - B
-# 9 - (Q), G
-# 0 - O
-# ['I', 'Í', 'Z', 'E', 'É', 'Ê', 'A', 'Á', 'Ã', 'S', 'B', 'T', 'B', 'Q', 'O', 'Ó', 'Õ']
-
-letras = set('IÍZEÉÊAÁÃSBTBQOÓÕ')
-
-
-def chopchopleet(dic):
-    return [word for word in dic if any(i in word[3] for i in letras) and any(i in word[5] for i in letras) and any(i in word[6] for i in letras)]
-
+from fun import *
 
 dicionario = "data\pt-BR.dic"
 
@@ -38,9 +7,21 @@ data = open(dicionario, encoding="utf8").readlines()
 for i in range(len(data)):
     data[i] = chopchopextra(data[i])
 
+for i in range(len(data)):
+    data[i] = chopchopacentos(data[i])
+
 seteletras = chopchopsete(data)
 
 leet = chopchopleet(seteletras)
 
+for i in range(len(leet)):
+    leet[i] = final(leet[i])
+
 for word in leet:
     print(word)
+
+save = open("placas.txt", "w")
+
+
+for word in leet:
+    save.write(word+'\n')
