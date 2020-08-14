e5adaa2577ee1b79a3a56e57aad963f6b1077baf
João Hirasawa <joaohirasawaa@gmail.com>
2020-08-14 14:46:28 -0300

    Generator changes to output JSON

--- src/generator.py
+++ src/generator.py
@@ -1,5 +1,6 @@
 import processing
 import os
+import json
 
 associations = {'A': '4', 'B': '8', 'E': '3', 'G': '6',
                 'I': '1', 'O': '0', 'Q': '9', 'S': '5', 'T': '7', 'Z': '2'}
@@ -14,17 +15,11 @@ lang = dic.split('.')[0]
 with open(os.path.join(path, 'dic', dic), 'r', encoding='utf8') as f:
     data = f.read().splitlines()
 
-clean = dict((w, i) for i, w in enumerate(map(processing.clean_and_normalize_word, data))
-             if processing.has_seven_alphabet_chars(w) and processing.is_match(w, associations))
+clean = {w: i for i, w in enumerate(map(processing.clean_and_normalize_word, data))
+         if processing.has_seven_alphabet_chars(w) and processing.is_match(w, associations)}
 
-final = [processing.convert_to_leet(i, associations) for i in clean.keys()]
+final = dict(zip([processing.convert_to_leet(i, associations) for i in clean.keys()], [
+             processing.clean_word(data[i]).upper() for i in clean.values()]))
 
-originals = [processing.clean_word(data[i]).upper() for i in clean.values()]
-
-with open(os.path.join(path, 'plates', lang + '-plates.txt'), 'w', encoding='utf8') as f:
-    for word in final:
-        f.write(word + '\n')
-
-with open(os.path.join(path, 'plates', lang + '-words.txt'), 'w', encoding='utf8') as f:
-    for word in originals:
-        f.write(word + '\n')
+with open(os.path.join(path, 'plates', lang + '.json'), 'w', encoding='utf8') as f:
+    json.dump(final, f)
